f2836a26bd3075aca095335a61992da4b06f52c6
aki0921 <66670058+aki0921@users.noreply.github.com>
2022-05-24 06:43:26 +0900

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,4 @@
 const btn = document.getElementById('navBtn');
-const link = document.getElementsByClassName('scrollLink');
 
 btn.addEventListener('click',function() {
   document.getElementById('nav').classList.toggle('show');
@@ -8,6 +7,13 @@ btn.addEventListener('click',function() {
   document.getElementById('btnLine2').classList.toggle('rotate2');
 });
 
-link.addEventListener('click',function() {
-  document.getElementById('nav').classList.remove('show');
-});
+const linkText = document.getElementsByTagName('nav')[0].getElementsByTagName('a');
+for (var n of linkText) {
+  n.addEventListener('click',function() {
+    document.getElementById('nav').classList.remove('show');
+    document.getElementById('navBtn').classList.remove('bgAlpha');
+    document.getElementById('btnLine1').classList.remove('rotate1');
+    document.getElementById('btnLine2').classList.remove('rotate2');
+    console.log('ok');
+  });
+}
